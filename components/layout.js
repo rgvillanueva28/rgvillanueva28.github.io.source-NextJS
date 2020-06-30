7a46e1b459ca2e876f77da955ba1c672a40aa67b
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-06-30 22:41:23 +0800

    Added header

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,31 @@
-import { Layout } from "antd";
+import { Layout, Menu } from "antd";
+import Link from "next/link";
 
-const { Content } = Layout;
+const { Content, Header } = Layout;
 
 export default function LayoutComponent({ children }) {
-  return <Content>{children}</Content>;
+  return (
+    <Layout className="layout">
+      <Header>
+        <Menu mode="horizontal" theme="dark">
+          <Menu.Item key="1">
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      {children}
+    </Layout>
+  );
 }

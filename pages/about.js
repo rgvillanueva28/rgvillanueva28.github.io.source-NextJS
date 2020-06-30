058a82eec445d4033dd4a5a40ef7b774299fb826
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-06-30 22:43:45 +0800

    Added about and contact page

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import LayoutComponent from "../components/layout";
+
+export default function About() {
+  return (
+    <LayoutComponent>
+      <h1>About page</h1>
+    </LayoutComponent>
+  );
+}
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import LayoutComponent from "../components/layout";
+
+export default function Contact() {
+  return (
+    <LayoutComponent>
+      <h1>Contact page</h1>
+    </LayoutComponent>
+  );
+}

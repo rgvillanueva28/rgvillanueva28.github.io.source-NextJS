1451390e782d1aa193e3714e40f46f81646b92d4
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-08-27 00:52:13 +0800

    🔁Changed page animation from _app.js to layout.js

--- components/layout.js
+++ components/layout.js
@@ -2,26 +2,26 @@ import Head from "next/head";
 import Header from "../components/header";
 
 import { useState, useEffect, Children, cloneElement } from "react";
+import { motion } from "framer-motion";
 
 export default function LayoutComponent({ children }) {
   let listener = null;
   const [onTop, setOnTop] = useState(true);
 
   useEffect(() => {
-    listener = document.addEventListener("scroll", e => {
-      var scrolled = document.scrollingElement.scrollTop
+    listener = document.addEventListener("scroll", (e) => {
+      var scrolled = document.scrollingElement.scrollTop;
       if (scrolled >= 60) {
-        setOnTop(false)
+        setOnTop(false);
       } else {
-        setOnTop(true)
+        setOnTop(true);
       }
-    })
+    });
 
     return () => {
-      document.removeEventListener("scroll", listener)
-    }
-
-  }, [onTop])
+      document.removeEventListener("scroll", listener);
+    };
+  }, [onTop]);
 
   return (
     <div className="">
@@ -37,7 +37,25 @@ export default function LayoutComponent({ children }) {
         ></link>
       </Head>
       <Header onTop={onTop} />
-      {children}
+      <motion.div
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+            transition: {
+              when: "beforeChildren",
+              staggerChildren: 0.05,
+              delayChildren: 0.2,
+            },
+          },
+        }}
+      >
+        {children}
+      </motion.div>
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,6 @@
 import "../styles/index.css";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-export default function MyApp({ Component, pageProps, router }) {
-  return (
-  <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
-    pageInitial:{
-      opacity: 0,
-    },
-    pageAnimate: {
-      opacity: 1,
-      transition: {
-        when: "beforeChildren",
-        staggerChildren: 0.05,
-        delayChildren: 0.2
-      },
-    }
-  }}>
-    <Component {...pageProps} />
-  </motion.div>
-  )
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }

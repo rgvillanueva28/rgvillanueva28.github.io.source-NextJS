a44fe3a6c30dcce1c1e82c53fa1b2c4bc9d74cae
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-10-20 00:32:08 +0800

    🔨Fixed portfolio card category overflow

--- components/portfolioItem.tsx
+++ components/portfolioItem.tsx
@@ -9,12 +9,12 @@ export default function PortfolioItem({
 }: any) {
   return (
     <div className="flex flex-wrap w-full px-4 py-4 md:w-1/2 lg:w-1/3">
-      <div className="flex flex-col items-stretch min-h-full bg-accent-dark p-6">
+      <div className="flex flex-col items-stretch w-full min-h-full bg-accent-dark p-6">
         <h4 className="text-foreground mb-4">{title}</h4>
         <p className="text-accent-light mb-10">{description}</p>
-        <div className="flex space-x-2 text-accent-light text-sm mb-4">
+        <div className="text-accent-light text-sm mb-4">
           {tags.map((tag: any) => (
-            <span className="bg-accent-mid rounded-lg p-2">{tag}</span>
+            <span className="inline-block bg-accent-mid rounded-lg p-2 mr-2 mb-2">{tag}</span>
           ))}
         </div>
         <div className="flex flex-row space-x-4 text-accent-light ml-auto mt-auto">

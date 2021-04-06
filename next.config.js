27085e2f2d3f6302ebcd442c575c6ad5b103fd9b
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2021-04-06 23:12:37 +0800

    Updated packages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const isDev = process.env.NODE_ENV === "development"
 const withPWA = require('next-pwa')
  
 module.exports = withPWA({
+    future: {
+        webpack5: true,
+    },
     pwa: {
         disable: isDev,
         dest: 'public'
